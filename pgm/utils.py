567b7d9a772834e7ca04194a882f5ed901aeda5f
Papa Moussa THIOUNE <pmthioune@nexialog.com>
2025-01-16 11:08:22 +0100

    Update

--- pgm/utils.py
+++ pgm/utils.py
@@ -1,38 +1,17 @@
-import os
-import pandas as pd
+def process_indicators(instances, rating_system):
+    for instance in instances:
+        instance[rating_system].get_indicator()
 
-
-def load_named_ranges_from_csv(folder_path, period=None):
-    """
-    Charge les fichiers CSV dont les noms commencent par 'ind' et ajoute la période aux clés du dictionnaire si spécifiée.
-
-    Args:
-        folder_path (str): Chemin du dossier contenant les fichiers CSV.
-        period (str, optional): Période à inclure dans les noms des clés du dictionnaire (exemple : '2024', 'Q1'). Par défaut, None.
-
-    Returns:
-        dict: Dictionnaire où les clés sont les noms des plages nommées (avec ou sans période) et les valeurs sont des DataFrames.
-    """
-    named_ranges_dict = {}
-
-    # Parcourir les fichiers CSV dans le dossier
-    for file_name in os.listdir(folder_path):
-        # Vérifier si le fichier commence par "ind" et a l'extension ".csv"
-        if file_name.startswith("ind") and file_name.endswith(".csv"):
-            # Nom de la clé sans extension
-            base_key = os.path.splitext(file_name)[0]
-
-            # Ajouter la période au nom de la clé si spécifiée
-            if period:
-                key = f"{base_key}_{period}"
-            else:
-                key = base_key
-
-            # Charger le fichier CSV en DataFrame
-            file_path = os.path.join(folder_path, file_name)
-            df = pd.read_csv(file_path)
-
-            # Ajouter au dictionnaire
-            named_ranges_dict[key] = df
-
-    return named_ranges_dict
+# Appel de la fonction
+process_indicators(
+    [
+        ind1_instance,
+        ind2_instance,
+        ind3_instance,
+        indő_instance,
+        ind7_instance,
+        ind8_instance,
+        ind9_instance
+    ],
+    rating_system
+)
