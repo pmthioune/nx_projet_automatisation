845a9b86de8964edb5e65d521e573616784fbd48
Fayçal <fousseini@nexialog.com>
2025-02-17 17:49:20 +0100

    Update dash data_quality_report

--- pgm/assets/data_quality_ui.py
+++ pgm/assets/data_quality_ui.py
@@ -5,16 +5,44 @@ import dash_bootstrap_components as dbc
 data_quality_content = html.Div(
     [
         html.H3("Data Quality Analysis", style={"color": "#FF5733", "margin-bottom": "20px"}),
-        dcc.Dropdown(id='variable-selector', options=[], multi=True, placeholder="Select variables"),
+
+        # KPI Section
+        html.Div(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(html.Div(id='total-missing-values', className='kpi-box'), width=3),
+                        dbc.Col(html.Div(id='total-duplicates', className='kpi-box'), width=3),
+                        dbc.Col(html.Div(id='total-outliers', className='kpi-box'), width=3),
+                        dbc.Col(html.Div(id='timeliness', className='kpi-box'), width=3),
+                    ],
+                    className="kpi-row"
+                ),
+            ],
+            className="kpi-section"
+        ),
+
+        # Graphs for data quality metrics
         dcc.Graph(id='missing-data-graph'),
         dcc.Graph(id='outliers-detection'),
         dcc.Graph(id='duplicates-graph'),
         dcc.Graph(id='data-distribution-graph'),
         dcc.Graph(id='correlation-matrix'),
+
         html.H4("Detailed DQ Report", style={"color": "#FF5733", "margin-top": "20px"}),
-        html.H4("Résumé des variables sélectionnées", style={"textAlign": "center", "color": "#2c3e50"}),
+
+        # Dropdown for selecting variables
+        dcc.Dropdown(id='variable-selector', options=[], multi=True, placeholder="Select variables"),
+
+        html.H4("DQ Sum up", style={"textAlign": "center", "color": "#2c3e50"}),
+
+        # Summary of selected variables
         html.Div(id='variables-summary'),
+
+        # Data quality report
         html.Div(id='data-quality-report'),
+
+        # Data quality table
         dash_table.DataTable(
             id='data-quality-table',
             columns=[],
@@ -36,6 +64,33 @@ data_quality_content = html.Div(
                 {'if': {'row_index': 'odd'}, 'backgroundColor': 'rgb(40, 40, 40)'}
             ]
         ),
+
+        html.H4("Statistiques Descriptives", style={"color": "#FF5733", "margin-top": "20px"}),
+
+        # Descriptive statistics table
+        dash_table.DataTable(
+            id='descriptive-stats-table',
+            columns=[],
+            data=[],
+            style_table={'overflowX': 'auto'},
+            style_header={
+                'backgroundColor': 'rgb(30, 30, 30)',
+                'color': 'white',
+                'fontWeight': 'bold',
+                'textAlign': 'center'
+            },
+            style_cell={
+                'backgroundColor': 'rgb(50, 50, 50)',
+                'color': 'white',
+                'textAlign': 'left',
+                'padding': '10px'
+            },
+            style_data_conditional=[
+                {'if': {'row_index': 'odd'}, 'backgroundColor': 'rgb(40, 40, 40)'}
+            ]
+        ),
+
+        # Button to download the report
         dbc.Button(
             "Télécharger le rapport",
             id="download-report",
--- pgm/callbacks.py
+++ pgm/callbacks.py
@@ -6,6 +6,7 @@ from dash import html, dcc
 from assets.data_import import load_data
 from assets.data_quality import data_quality_report, save_data_quality_report
 import plotly.express as px
+from main import start_process, get_progress
 
 OUTPUT_DIR = '../../output'
 if not os.path.exists(OUTPUT_DIR):
@@ -46,40 +47,154 @@ def register_callbacks(app):
                   Input('language-toggle', 'n_clicks'),
                   State('language-store', 'data'))
     def toggle_language(n_clicks, current_language):
-        LANGUAGES = {
-            "fr": "Français",
-            "en": "English"
-        }
         if n_clicks is None:
             return current_language, LANGUAGES[current_language]
         new_language = "en" if current_language == "fr" else "fr"
         return new_language, LANGUAGES[new_language]
 
     @app.callback(
-        [Output("progress-bar-datapack", "value"),
-         Output("log-output-datapack", "children"),
-         Output("btn-download-datapack", "style"),
-         Output("download-link", "href"),
-         Output("interval-datapack", "disabled")],
-        [Input("btn-start-datapack", "n_clicks"), Input("interval-datapack",
-                                                        "n_intervals")]
+        [
+            Output('total-missing-values', 'children'),
+            Output('total-duplicates', 'children'),
+            Output('total-outliers', 'children'),
+            Output('timeliness', 'children'),
+            Output('missing-data-graph', 'figure'),
+            Output('data-distribution-graph', 'figure'),
+            Output('outliers-detection', 'figure'),
+            Output('correlation-matrix', 'figure'),
+            Output('duplicates-graph', 'figure'),
+            Output('data-quality-table', 'columns'),
+            Output('data-quality-table', 'data'),
+            Output('variable-selector', 'options'),
+            Output('variables-summary', 'children'),
+            Output('descriptive-stats-table', 'columns'),
+            Output('descriptive-stats-table', 'data')
+        ],
+        [Input('url', 'pathname'), Input('variable-selector', 'value')]
+    )
+    def generate_data_quality_report(pathname, selected_variables):
+        if pathname == "/dataquality":
+            try:
+                # Charger les données
+                df = load_data(input_file)
+                # Générer le rapport de qualité des données
+                report = data_quality_report(df, id_column='id')
+                # Sélectionner uniquement les colonnes numériques
+                numeric_df = df.select_dtypes(include='number')
+                # Options pour le dropdown
+                variable_options = [{'label': col, 'value': col} for col in df.columns]
+                # Enregistrer le rapport de qualité des données
+                save_data_quality_report(report)
+
+                # KPI values
+                total_rows = len(df)
+                total_missing_values = f"Total Missing Values: {report['total_missing']} ({(report['total_missing'] / (total_rows * len(df.columns))) * 100:.2f}%)"
+                total_duplicates = f"Total Duplicates: {report['duplicates']} ({(report['duplicates'] / total_rows) * 100:.2f}%)"
+                total_outliers = f"Total Outliers: {report['outliers'].sum()} ({(report['outliers'].sum() / total_rows) * 100:.2f}%)"
+                timeliness = f"Timeliness (days): {report['timeliness']}"
+
+                # Graphique des valeurs manquantes
+                missing_data_fig = px.bar(
+                    report['missing_per_variable'].reset_index(),
+                    x='index',
+                    y=0,
+                    title='Missing Values per Variable'
+                )
+
+                # Graphique de détection des outliers
+                outliers_detection_fig = px.box(numeric_df, title='Outliers Detection')
+                # Graphique de distribution des données
+                data_distribution_fig = px.histogram(numeric_df, title='Data Distribution')
+                # Matrice de corrélation
+                correlation_matrix_fig = px.imshow(numeric_df.corr(), title='Correlation Matrix')
+
+                # Graphique des doublons
+                duplicates_fig = px.bar(
+                    pd.DataFrame({
+                        'Variable': ['Total Duplicates', 'Duplicates by ID'],
+                        'Count': [report['duplicates'], report['duplicates_by_id']]
+                    }),
+                    x='Variable',
+                    y='Count',
+                    title='Duplicate Values'
+                )
+
+                # Résumé des variables sélectionnées
+                if not selected_variables:
+                    summary = "Aucune variable sélectionnée."
+                    descriptive_stats_columns = []
+                    descriptive_stats_data = []
+                else:
+                    summary = [html.P(f"Variable: {var}") for var in selected_variables]
+                    desc_df = df[selected_variables].describe().reset_index()
+                    summary.append(html.P(desc_df.to_string()))
+
+                    descriptive_stats_columns = [{"name": i, "id": i} for i in desc_df.columns]
+                    descriptive_stats_data = desc_df.to_dict('records')
+
+                # Colonnes et données pour le tableau de qualité des données
+                data_quality_table_columns = [
+                    {"name": "Variable", "id": "Variable"},
+                    {"name": "Missing Values", "id": "Missing Values"},
+                    {"name": "Duplicates", "id": "Duplicates"}
+                ]
+                data_quality_table_data = [
+                    {"Variable": var, "Missing Values": val,
+                     "Duplicates": report['duplicates_by_id'] if var == 'id' else ''}
+                    for var, val in report['missing_per_variable'].items()
+                ]
+
+                return (
+                    total_missing_values,
+                    total_duplicates,
+                    total_outliers,
+                    timeliness,
+                    missing_data_fig,
+                    data_distribution_fig,
+                    outliers_detection_fig,
+                    correlation_matrix_fig,
+                    duplicates_fig,
+                    data_quality_table_columns,
+                    data_quality_table_data,
+                    variable_options,
+                    summary,
+                    descriptive_stats_columns,
+                    descriptive_stats_data
+                )
+            except Exception as e:
+                print(f"Error generating data quality report: {e}")
+                return str(e), {}, {}, {}, {}, {}, [], [], [], [], [], [], [], []
+        return {}, {}, {}, {}, {}, {}, [], [], [], [], [], [], [], []
+
+    @app.callback(
+        [
+            Output("progress-bar-datapack", "value"),
+            Output("log-output-datapack", "children"),
+            Output("btn-download-datapack", "style"),
+            Output("download-link", "href"),
+            Output("interval-datapack", "disabled")
+        ],
+        [Input("btn-start-datapack", "n_clicks"), Input("interval-datapack", "n_intervals")]
     )
     def update_progress(n_clicks, n_intervals):
-        from main import start_process, get_progress
-
         triggered_id = ctx.triggered_id
 
         if triggered_id == "btn-start-datapack":
+            # Démarrer le processus dans un thread séparé
             thread = threading.Thread(target=start_process)
             thread.start()
+
+            # Initialement, afficher le statut de traitement
             return 0, "🚀 Traitement en cours...", {"display": "none"}, "", False
 
         elif triggered_id == "interval-datapack":
-            progress_state = get_progress()
+            # Simuler la progression à des fins de démonstration
+            progress_state = get_progress()  # Cela devrait appeler votre fonction de suivi de progression réelle
             progress = progress_state["progress"]
             message = progress_state["message"]
 
             if progress == 100:
+                # Afficher le bouton de téléchargement lorsque terminé
                 return (
                     progress,
                     message,
@@ -92,90 +207,6 @@ def register_callbacks(app):
             return progress, message, {"display": "none"}, "", False
 
         return no_update, no_update, no_update, no_update, no_update
-
-    @app.callback(
-        [
-            Output('data-quality-report', 'children'),
-            Output('missing-data-graph', 'figure'),
-            Output('data-distribution-graph', 'figure'),
-            Output('outliers-detection', 'figure'),
-            Output('correlation-matrix', 'figure'),
-            Output('duplicates-graph', 'figure'),
-            Output('data-quality-table', 'columns'),
-            Output('data-quality-table', 'data'),
-            Output('variable-selector', 'options'),
-            Output('variables-summary', 'children')
-        ],
-        [Input('url', 'pathname'), Input('variable-selector', 'value')]
-    )
-    def generate_data_quality_report(pathname, selected_variables):
-        if pathname == "/dataquality":
-            try:
-                df = load_data(input_file)
-                report = data_quality_report(df, id_column='id')
-                numeric_df = df.select_dtypes(include='number')
-                variable_options = [{'label': col, 'value': col} for col in df.columns]
-                save_data_quality_report(report)
-
-                missing_data_df = pd.DataFrame({
-                    'Variable': report['missing_per_variable'].index,
-                    'Missing Values': report['missing_per_variable'].values
-                }).sort_values(by='Missing Values', ascending=False)
-                missing_data_fig = px.bar(
-                    missing_data_df,
-                    x='Variable',
-                    y='Missing Values',
-                    title='Missing Values per Variable'
-                )
-
-                outliers_detection_fig = px.box(numeric_df, title='Outliers Detection')
-                data_distribution_fig = px.histogram(numeric_df, title='Data Distribution')
-                correlation_matrix_fig = px.imshow(numeric_df.corr(), title='Correlation Matrix')
-
-                duplicates_df = pd.DataFrame({
-                    'Variable': ['Total Duplicates', 'Duplicates by ID'],
-                    'Count': [report['duplicates'], report['duplicates_by_id']]
-                })
-                duplicates_fig = px.bar(
-                    duplicates_df,
-                    x='Variable',
-                    y='Count',
-                    title='Duplicate Values'
-                )
-
-                if not selected_variables:
-                    summary = "Aucune variable sélectionnée."
-                else:
-                    summary = [html.P(f"Variable: {var}") for var in selected_variables]
-                    desc_df = df[selected_variables].describe().reset_index()
-                    summary.append(html.P(desc_df.to_string()))
-
-                data_quality_table_columns = [
-                    {"name": "Variable", "id": "Variable"},
-                    {"name": "Missing Values", "id": "Missing Values"},
-                    {"name": "Duplicates", "id": "Duplicates"}
-                ]
-                data_quality_table_data = [
-                    {"Variable": var, "Missing Values": val, "Duplicates": report['duplicates_by_id'] if var == 'id' else ''}
-                    for var, val in report['missing_per_variable'].items()
-                ]
-
-                return (
-                    f"Total Missing Values: {report['total_missing']}",
-                    missing_data_fig,
-                    data_distribution_fig,
-                    outliers_detection_fig,
-                    correlation_matrix_fig,
-                    duplicates_fig,
-                    data_quality_table_columns,
-                    data_quality_table_data,
-                    variable_options,
-                    summary
-                )
-            except Exception as e:
-                print(f"Error generating data quality report: {e}")
-                return str(e), {}, {}, {}, {}, {}, [], [], [], []
-        return {}, {}, {}, {}, {}, {}, [], [], [], []
 
     @app.callback(
         Output("download-report", "data"),
